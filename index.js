085911b01a9958f7cbdeee4b629cd91d5c3a48f2
Chris Freeman <cfreeman.code@gmail.com>
2017-07-09 15:56:26 -0500

    shim swipe.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
 /* eslint-env node */
 'use strict';
 
 module.exports = {
-  name: 'ember-swipe'
+  name: 'ember-swipe',
+
+  included() {
+    this._super.included.apply(this, arguments);
+    this.import('vendor/swipe.js');
+    this.import('vendor/shims/swipe.js');
+  },
+
+  treeForVendor(vendorTree) {
+    const momentTree = new Funnel(path.dirname(require.resolve('swipe/swipe.js')), {
+      files: ['swipe.js']
+    });
+
+    return new MergeTrees([vendorTree, momentTree]);
+  }
 };
