f07a5a89843658a57f2ed8179200359aea6f5236
Chris Freeman <cfreeman.code@gmail.com>
2017-07-10 09:05:12 -0500

    Set up dependencies

---
+++ vendor/shims/swipe.js
@@ -0,0 +1,12 @@
+(function() {
+  function vendorModule() {
+    'use strict';
+
+    return {
+      'default': self['Swipe'],
+      __esModule: true,
+    };
+  }
+
+  define('swipe-js-iso', [], vendorModule);
+})();
